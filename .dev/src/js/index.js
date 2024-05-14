2b7d3686671c5b796a4645df408dc891c590447e
mastronelli88@gmail.com <mastronelli88@gmail.com>
2024-05-14 07:58:31 -0300

    build

--- .dev/src/js/index.js
+++ .dev/src/js/index.js
@@ -12,6 +12,10 @@ $(".slider-home").slick({
 	infinite: false,
 	autoplay: true,
 	autoplaySpeed: 2500,
+	arrows: true,
+	prevArrow: $(".prev-h"),
+	nextArrow: $(".next-h"),
+	dots: true,
 })
 
 $("#phone").mask("(00) 00000-0000")
